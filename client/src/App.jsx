3ff7394ea6b484cad921d9577205214143351b41
pavanmanishd <pavanmanishd@gmail.com>
2024-02-03 13:13:26 +0530

    Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { Routes, useNavigate } from "react-router-dom";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+import variables from "./config/index";
 import "./App.css";
 
-// const CLIENT_SERVER_URL = "http://localhost:3000";
+const clerkPubKey = variables.VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
-
-  useEffect(() => {
-    const status = localStorage.getItem("isLogged");
-    if (status === "true") {
-      setIsLogged(true);
-    } else {
-      setIsLogged(false);
-    }
-
-    if(!isLogged){
-      window.location.href = `/login`;
-    }
-  }, [isLogged]);
-
-  return <></>;
+  const navigate = useNavigate();
+  return (
+    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+      <SignedIn>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
-
 export default App;
---
+++ client/src/config/index.js
@@ -0,0 +1,6 @@
+const variables = 
+{
+    "VITE_CLERK_PUBLISHABLE_KEY":"pk_test_cmVsYXhlZC1ha2l0YS01Ni5jbGVyay5hY2NvdW50cy5kZXYk"
+}
+
+export default variables;
