c9c64744f7bd3b5c915faa4d8f5de13235475768
pavanmanishd <pavanmanishd@gmail.com>
2024-02-04 08:03:00 +0530

    Add sign up functionality and landing page

--- client frontend/src/App.jsx
+++ client frontend/src/App.jsx
@@ -4,11 +4,15 @@ import Renders from "./pages/Renders";
 import { Routes, useNavigate } from "react-router-dom";
 import {
   ClerkProvider,
+  RedirectToSignIn,
+  RedirectToSignUp,
   SignedIn,
   SignedOut,
-  RedirectToSignIn,
+  SignIn,
+  SignUp,
 } from "@clerk/clerk-react";
 import variables from "./config/index";
+import Landing from "./pages/Landing";
 
 const clerkPubKey = variables.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -23,7 +27,12 @@ function App() {
         </Routes>
       </SignedIn>
       <SignedOut>
-        <RedirectToSignIn />
+        {/* <RedirectToSignIn /> */}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<RedirectToSignIn />} />
+          <Route path="/signup" element={<RedirectToSignUp />} />
+        </Routes>
       </SignedOut>
     </ClerkProvider>
   );
---
+++ client frontend/src/pages/Landing.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import "../styles/Landing.css";
+import Main from "../assets/main.png";
+import { Link } from "react-router-dom";
+function Landing() {
+  const handleStart = () => {
+    window.location.href = "/signup";
+  };
+  return (
+    <div className="landing_main">
+      <div className="nav">
+        <div>
+          <h1>1o1</h1>
+        </div>
+        <div>
+          {/* <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link> */}
+          <button onClick={() => (window.location.href = "/login")}>
+            Login
+          </button>
+          <button onClick={() => (window.location.href = "/signup")}>
+            Sign Up
+          </button>
+        </div>
+      </div>
+      <div className="main_cont">
+        <div className="main_info">
+          <h2>Parallel Pilot</h2>
+          <p>Empowering Computational Tasks with Peer-to-Peer Efficiency</p>
+          <button onClick={handleStart}>Get Started</button>
+        </div>
+        <div className="img_cont">
+          <img src={Main} alt="img" className="img" />
+        </div>
+      </div>
+      {/* <div className="scroll">
+        <p>Scroll Down</p>
+      </div> */}
+      <div className="reason_cont">
+        <h2>Why Parallel Pilot?</h2>
+        <div className="reason_main">
+          <div className="reason">
+            <h3>Cost-Effective</h3>
+            <p>
+              Parallel Pilot is a cost-effective solution to reduce
+              computational processing time.
+            </p>
+          </div>
+          <div className="reason">
+            <h3>Distributed</h3>
+            <p>
+              Our platform is designed to distribute tasks across multiple
+              workers, reducing processing time.
+            </p>
+          </div>
+          <div className="reason">
+            <h3>Scalable</h3>
+            <p>
+              Our platform is designed to scale with your computational needs.
+            </p>
+          </div>
+          <div className="reason">
+            <h3>Secure</h3>
+            <p>
+              We ensure that your data is secure and protected from unauthorized
+              access.
+            </p>
+          </div>
+          <div className="reason">
+            <h3>Efficient</h3>
+            <p>
+              Our platform is designed to optimize computational processing and
+              reduce time.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="working_main_cont">
+        <h2>How It Works</h2>
+        <div className="step_cont">
+            <div className="step">
+              <h3>1. Client Uploads Task</h3>
+              <p>
+                Upload your heavy computational task easily through our
+                user-friendly interface.
+              </p>
+            </div>
+            <div className="step">
+              <h3>2. Load Balancer Optimizes Workload</h3>
+              <p>
+                Our intelligent load balancer efficiently distributes tasks to
+                available workers.
+              </p>
+            </div>
+            <div className="step">
+              <h3>3. Worker Processing</h3>
+              <p>
+                Workers process tasks simultaneously, reducing processing time
+                significantly.
+              </p>
+            </div>
+            <div className="step">
+              <h3>4. Results Consolidation</h3>
+              <p>
+                Final results are consolidated and delivered to clients, ensuring
+                accelerated processing.
+              </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
